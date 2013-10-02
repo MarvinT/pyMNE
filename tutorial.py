8a72db63c9c19e2194a72fb87d2443e379f53c96
Marvin Thielk <nivram@berkeley.edu>
2013-10-02 12:30:08 -0700

    first commit of tutorial.py

---
+++ tutorial.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def main():
+    # generate white noise stimuli
+    # create gabor rf
+    # generate spike train
+    # use 1/(1+exp(v*x+c)) to generate spike train
+    # run MNE
+    # display recovered rf to generative rf
+
+if __name__ == '__main__':
+    main()
