22a3477eea650934419d10a6fc000c4b4fa26d67
Marvin Thielk <nivram@berkeley.edu>
2013-10-03 11:54:30 -0700

    tutorial.py generates random white noise stimuli and a gabor filter for a receptive field

--- tutorial.py
+++ tutorial.py
@@ -1,8 +1,42 @@
 import numpy as np
+import matplotlib.cm as cm
+import pylab
+
+def gabor_fn(bw=3,gamma=0.5,psi=0,lambd=6,theta=0):
+    # bw    = bandwidth, (1)
+    # gamma = aspect ratio, (0.5)
+    # psi   = phase shift, (0)
+    # lambd = wave length, (>=2)
+    # theta = angle in rad, [0 pi)
+ 
+    sigma = lambd/np.pi*np.sqrt(np.log(2)/2)*(2**bw+1)/(2**bw-1);
+    sigma_x = sigma;
+    sigma_y = sigma/gamma;
+
+    sz = 16;
+
+    x = range(np.floor(-sz/2).astype(int), np.floor(sz/2).astype(int))
+    y = range(np.floor(sz/2).astype(int), np.floor(-sz/2).astype(int), -1)
+    xv, yv = np.meshgrid(x, y)
+    # x (right +)
+    # y (up +)
+
+    # Rotation 
+    x_theta=xv*np.cos(theta)+yv*np.sin(theta);
+    y_theta=-xv*np.sin(theta)+yv*np.cos(theta);
+
+    gb=np.exp(-0.5*(x_theta**2/sigma_x**2+y_theta**2/sigma_y**2))*np.cos(2*np.pi/lambd*x_theta+psi);
+    return gb
 
 def main():
-    # generate white noise stimuli
+    # generate white noise stimuli:
+    #   consider 2^14 16x16 white noise patches, 
+    #   reshaped to form column vector of length 256
+    stimuli = np.random.randn(256, 2**14)
     # create gabor rf
+    gb = gabor_fn(theta=np.pi/4)
+    pylab.imshow(gb, cmap=cm.gray, interpolation="nearest")
+    pylab.show()
     # generate spike train
     # use 1/(1+exp(v*x+c)) to generate spike train
     # run MNE
