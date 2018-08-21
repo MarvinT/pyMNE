38a492a21016a1a46c75483084aeec3761e1d8d3
Marvin T <MarvinT@users.noreply.github.com>
2018-08-20 17:22:24 -0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='pymne',
+    packages=find_packages(),
+    version='0.0.1',
+    description='Python implementation of Maximum Noise Entropy',
+    author='Marvin Thielk',
+    license='BSD2',
+)
